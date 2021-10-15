143b6763e955ef89f4a501e9dbc4797db560f3db
Juan I. Carlis <juancarlis93@gmail.com>
2021-10-15 20:53:09 -0300

    basic project tree

--- new-project.sh
+++ new-project.sh
@@ -4,8 +4,19 @@ projectName=$1
 
 echo "The name of the project is $projectName"
 
-
-echo "Making the project tree"
+echo "Building project folder tree"
 mkdir $projectName
 mkdir $projectName/$projectName
+mkdir $projectName/$projectName/data
+mkdir $projectName/$projectName/data/raw
+mkdir $projectName/$projectName/data/processed
+mkdir $projectName/$projectName/notebooks
 
+touch $projectName/$projectName/main.py
+touch $projectName/$projectName/README.md
+touch $projectName/$projectName/data/raw/.gitkeep
+touch $projectName/$projectName/data/processed/.gitkeep
+
+echo "Initialize git repository"
+cd $projectName/$projectName
+git init
